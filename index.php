8eff6586040d763b6ae7319b1a39cdb920fe946c
satishNunakana <76905574+satishNunakana@users.noreply.github.com>
2021-02-17 16:44:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2Hi This is my new Application</h2>";
-echo "Welcomw to the world of AWS<br>";
+echo "Welcome to the world of AWS<br>";
 echo "AWS Pipelining<br>";
-echo "Satish kumar nunakana";
+echo "Satish kumar nunakana-Bindu";
 ?>
